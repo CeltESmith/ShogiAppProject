db53d5300c7f5c5e00ddae89bb3f05a80cf361ca
Elijah Smith <elijah.d.s@outlook.com>
2023-09-17 19:29:06 -0400

    Add a class for the SilverGeneral

---
+++ ShogiLogic/Pieces/SilverGeneral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class SilverGeneral : Piece
+    {
+        public override PieceType Type => PieceType.SilverGeneral;
+        public override Player Color { get; }
+
+        public SilverGeneral(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            SilverGeneral copy = new SilverGeneral(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
