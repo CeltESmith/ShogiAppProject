d546bf2c7020507fe50fcee8e331cd852348e0a3
Elijah Smith <elijah.d.s@outlook.com>
2023-09-17 10:23:31 -0400

    Adding Abstract methodology to be used by each pieces code.

---
+++ ShogiLogic/Pieces/Piece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic
+{
+    public abstract class Piece
+    {
+        public abstract PieceType Type { get; }
+        public abstract Player Color { get; }
+        public bool HasMoverd { get; set; } = false;
+    }
+}
