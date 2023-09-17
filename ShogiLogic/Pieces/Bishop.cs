771b49f67cab81d22c66fab4b084d94d29d5ae1a
Elijah Smith <elijah.d.s@outlook.com>
2023-09-17 19:17:04 -0400

    Creating the Bishop class for the shogi project.

---
+++ ShogiLogic/Pieces/Bishop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class Bishop : Piece
+    {
+        public override PieceType Type => PieceType.Bishop;
+        public override Player Color { get; }
+
+        public Bishop(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Bishop copy = new Bishop(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
