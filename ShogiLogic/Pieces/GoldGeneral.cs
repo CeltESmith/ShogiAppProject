ed5741770386b372d8cc10aae59d386f620b0325
Elijah Smith <elijah.d.s@outlook.com>
2023-09-17 19:23:41 -0400

    Added the GoldGeneral Class to the project

---
+++ ShogiLogic/Pieces/GoldGeneral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class GoldGeneral : Piece
+    {
+        public override PieceType Type => PieceType.GoldGeneral;
+        public override Player Color { get; }
+
+        public GoldGeneral(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            GoldGeneral copy = new GoldGeneral(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
