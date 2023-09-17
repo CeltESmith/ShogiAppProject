282d9acbee6491b96dc5b429668f20eee8bb0eda
Elijah Smith <elijah.d.s@outlook.com>
2023-09-17 19:16:22 -0400

    Creating the pawn class for the pawn piece

--- ShogiLogic/Pieces/Pawn.cs
+++ ShogiLogic/Pieces/Pawn.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShogiLogic
 {
-    public class Pawn
+    public class Pawn : Piece
     {
+        public override PieceType Type => PieceType.Pawn;
+        public override Player Color { get; }
 
+        public Pawn(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Pawn copy = new Pawn(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
     }
 }
