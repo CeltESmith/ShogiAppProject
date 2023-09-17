3a04442465211ce0f41b71d574cfb4061b570bcf
Elijah Smith <elijah.d.s@outlook.com>
2023-09-16 21:19:31 -0400

    Creating a position class to see where the pieces are and their relation to the board.

---
+++ ShogiLogic/Position.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic
+{
+    public class Position
+    {
+        public int RowY { get; }
+        public int ColumnX { get; }
+
+        public Position(int rowY, int columnX)
+        {
+            RowY = rowY;
+            ColumnX = columnX;
+        }
+
+        public Player SquareColor()
+        {
+            if ((RowY +ColumnX) % 2 == 0)
+            {
+                return Player.White;
+            }
+            return Player.Black;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   RowY == position.RowY &&
+                   ColumnX == position.ColumnX;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowY, ColumnX);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+}
