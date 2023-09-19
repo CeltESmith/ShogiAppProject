47afe4b23f6ff3ec352576b16d8f93104539321e
Elijah Smith <elijah.d.s@outlook.com>
2023-09-19 08:42:48 -0400

    Add the Rook piece Class.

---
+++ ShogiLogic/Pieces/Rook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class Rook : Piece
+    {
+        public override PieceType Type => PieceType.Rook;
+        public override Player Color { get; }
+        
+        public Rook(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Rook copy = new Rook(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
