19360e63cca015da50acc582c53d76bd000cef4e
Elijah Smith <elijah.d.s@outlook.com>
2023-09-19 08:41:56 -0400

    adding the Knight piece

---
+++ ShogiLogic/Pieces/Knight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class Knight : Piece
+    {
+        public override PieceType Type => PieceType.Knight;
+        public override Player Color { get; }
+
+        public Knight(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Knight copy = new Knight(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
