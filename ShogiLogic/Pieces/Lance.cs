d6940cccec48d6ccf3e91dfbaeec40c992d70608
Elijah Smith <elijah.d.s@outlook.com>
2023-09-19 08:42:21 -0400

    Adding class for the lance piece.

---
+++ ShogiLogic/Pieces/Lance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class Lance : Piece
+    {
+        public override PieceType Type => PieceType.Lance;
+        public override Player Color { get; }
+
+        public Lance(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Lance copy = new Lance(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
