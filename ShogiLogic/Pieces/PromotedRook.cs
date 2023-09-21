2ab10789ce084438790afeab3e9adede4fa72bb9
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 20:57:46 -0400

    Adding the PromotedRook Piece base logic.

---
+++ ShogiLogic/Pieces/PromotedRook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class PromotedRook : Piece
+    {
+        public override PieceType Type => PieceType.PromotedRook;
+        public override Player Color { get; }
+
+        public PromotedRook(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            PromotedRook copy = new PromotedRook(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
