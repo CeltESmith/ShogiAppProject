81860ce24277bc2b50f82654696af4b79b4344bf
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 20:57:16 -0400

    Adding the PromotedBishop Piece base logic.

---
+++ ShogiLogic/Pieces/PromotedBishop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic.Pieces
+{
+    public class PromotedBishop : Piece
+    {
+        public override PieceType Type => PieceType.PromotedBishop;
+        public override Player Color { get; }
+
+        public PromotedBishop(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            PromotedBishop copy = new PromotedBishop(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+    }
+}
