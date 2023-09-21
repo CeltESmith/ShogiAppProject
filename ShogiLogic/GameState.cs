669425a63c685057ad0d22baff89871328e01128
Elijah Smith <elijah.d.s@outlook.com>
2023-09-20 20:58:55 -0400

    Adding the GameState class to connect between the ui and the logic of the game.

---
+++ ShogiLogic/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic
+{
+    public class GameState
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        public GameState(Board board, Player player)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
