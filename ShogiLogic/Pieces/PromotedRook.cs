cce6807ef96a9f59583d3ac6f20813757620559c
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:52:32 -0400

    updated promoted Rook Class

--- ShogiLogic/Pieces/PromotedRook.cs
+++ ShogiLogic/Pieces/PromotedRook.cs
@@ -11,6 +11,21 @@ namespace ShogiLogic.Pieces
         public override PieceType Type => PieceType.PromotedRook;
         public override Player Color { get; }
 
+        private readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.East,
+            Direction.South,
+            Direction.West
+        };
+        private readonly Direction[] dir = new Direction[]
+        {
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.SouthWest,
+            Direction.NorthWest
+        };
+
         public PromotedRook(Player color)
         {
             Color = color;
@@ -21,6 +36,46 @@ namespace ShogiLogic.Pieces
             PromotedRook copy = new PromotedRook(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board, Direction dirs)
+        {
+            for (Position pos = from + dirs; Board.IsInside(pos); pos += dirs)
+            {
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+
+                Piece piece = board[pos];
+
+                if (piece.Color != Color)
+                {
+                    yield return pos;
+                }
+            }
+
+            foreach (Direction di in dir)
+            {
+                Position to = from + di;
+
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
