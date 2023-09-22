f965c3f81543a392c8a40d6471b5c9c7a57820af
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:50:59 -0400

    updating the Bishop Class

--- ShogiLogic/Pieces/Bishop.cs
+++ ShogiLogic/Pieces/Bishop.cs
@@ -11,6 +11,14 @@ namespace ShogiLogic.Pieces
         public override PieceType Type => PieceType.Bishop;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.SouthEast,
+            Direction.SouthWest
+        };
+
         public Bishop(Player color)
         {
             Color = color;
@@ -21,6 +29,11 @@ namespace ShogiLogic.Pieces
             Bishop copy = new Bishop(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
