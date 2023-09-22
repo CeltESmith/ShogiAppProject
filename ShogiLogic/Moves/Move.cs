cf5e466fc20824377513fb798525808e020446fe
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:49:06 -0400

    Adding Move Class

---
+++ ShogiLogic/Moves/Move.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic
+{
+    public abstract class Move
+    {
+        public abstract MoveType Type { get; }
+        public abstract Position FromPos { get; }
+        public abstract Position ToPos { get; }
+
+        public abstract void Execute(Board board);
+    }
+}
