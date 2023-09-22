89d06c197b8393c6fe55ce351982b99aea7ba694
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:53:49 -0400

    updating the Rook Class

--- ShogiLogic/Pieces/Rook.cs
+++ ShogiLogic/Pieces/Rook.cs
@@ -10,6 +10,14 @@ namespace ShogiLogic.Pieces
     {
         public override PieceType Type => PieceType.Rook;
         public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West
+        };
         
         public Rook(Player color)
         {
@@ -21,6 +29,11 @@ namespace ShogiLogic.Pieces
             Rook copy = new Rook(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
