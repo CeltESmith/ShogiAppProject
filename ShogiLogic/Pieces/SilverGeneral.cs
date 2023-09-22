ca1e76a217594348c5589176612114c22a79ac7f
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:53:17 -0400

    updating the SilverGeneral Class

--- ShogiLogic/Pieces/SilverGeneral.cs
+++ ShogiLogic/Pieces/SilverGeneral.cs
@@ -11,6 +11,8 @@ namespace ShogiLogic.Pieces
         public override PieceType Type => PieceType.SilverGeneral;
         public override Player Color { get; }
 
+        private static Direction[] dirs;
+
         public SilverGeneral(Player color)
         {
             Color = color;
@@ -21,6 +23,69 @@ namespace ShogiLogic.Pieces
             SilverGeneral copy = new SilverGeneral(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            if (Color == Player.White)
+            {
+                dirs = new Direction[]
+                {
+                    Direction.North,
+                    Direction.NorthEast,
+                    Direction.NorthWest,
+                    Direction.SouthEast,
+                    Direction.SouthWest
+                };
+                foreach (Direction dir in dirs)
+                {
+                    Position to = from + dir;
+
+                    if (!Board.IsInside(to))
+                    {
+                        continue;
+                    }
+
+                    if (board.IsEmpty(to) || board[to].Color != Color)
+                    {
+                        yield return to;
+                    }
+                }
+            }
+
+            else if (Color == Player.Black)
+            {
+                dirs = new Direction[]
+                {
+                    Direction.South,
+                    Direction.SouthWest,
+                    Direction.SouthEast,
+                    Direction.NorthEast,
+                    Direction.NorthWest
+                };
+                foreach (Direction dir in dirs)
+                {
+                    Position to = from + dir;
+
+                    if (!Board.IsInside(to))
+                    {
+                        continue;
+                    }
+
+                    if (board.IsEmpty(to) || board[to].Color != Color)
+                    {
+                        yield return to;
+                    }
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
         }
     }
 }
