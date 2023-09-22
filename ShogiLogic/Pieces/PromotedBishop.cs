738847660c188fc7e1a8efc7824587ff91eb2edc
Elijah Smith <elijah.d.s@outlook.com>
2023-09-22 09:51:46 -0400

    Update to PromotedBishop Class.

--- ShogiLogic/Pieces/PromotedBishop.cs
+++ ShogiLogic/Pieces/PromotedBishop.cs
@@ -11,6 +11,21 @@ namespace ShogiLogic.Pieces
         public override PieceType Type => PieceType.PromotedBishop;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.SouthEast,
+            Direction.SouthWest
+        };
+        private static readonly Direction[] dir = new Direction[]
+        {
+            Direction.North,
+            Direction.East,
+            Direction.South,
+            Direction.West
+        };
+
         public PromotedBishop(Player color)
         {
             Color = color;
@@ -21,6 +36,29 @@ namespace ShogiLogic.Pieces
             PromotedBishop copy = new PromotedBishop(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction di in dir)
+            {
+                Position to = from + di;
+
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to)); 
         }
     }
 }
