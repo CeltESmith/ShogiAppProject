73a09045922b3a3dbfc73cf9016c45692b01ecef
Elijah Smith <elijah.d.s@outlook.com>
2023-09-24 08:28:26 -0400

    king class updated

--- ShogiLogic/Pieces/King.cs
+++ ShogiLogic/Pieces/King.cs
@@ -11,6 +11,18 @@ namespace ShogiLogic.Pieces
         public override PieceType Type => PieceType.King;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.East,
+            Direction.South,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.SouthWest,
+            Direction.NorthWest
+        };
+
         public King(Player color)
         {
             Color = color;
@@ -21,6 +33,32 @@ namespace ShogiLogic.Pieces
             King copy = new King(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction dir in dirs)
+            {
+                Position to = from + dir;
+
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
         }
     }
 }
