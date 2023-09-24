d7dfb5f2da596b6c5e7e21a681c74c4614afac8c
Elijah Smith <elijah.d.s@outlook.com>
2023-09-24 08:29:49 -0400

    updating the pawn class

--- ShogiLogic/Pieces/Pawn.cs
+++ ShogiLogic/Pieces/Pawn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -12,9 +13,20 @@ namespace ShogiLogic
         public override PieceType Type => PieceType.Pawn;
         public override Player Color { get; }
 
+        private readonly Direction forward;
+
         public Pawn(Player color)
         {
             Color = color;
+
+            if (color == Player.White)
+            {
+                forward = Direction.North;
+            }
+            else if(color == Player.Black)
+            {
+                forward = Direction.South;
+            }
         }
 
         public override Piece Copy()
@@ -22,6 +34,26 @@ namespace ShogiLogic
             Pawn copy = new Pawn(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private bool CanMoveTo(Position pos, Board board)
+        {
+            return Board.IsInside(pos) && board.IsEmpty(pos) || board[pos].Color != Color;
+        }
+
+        private IEnumerable<Move> ForwardMove(Position from, Board board)
+        {
+            Position move = from + forward;
+
+            if (CanMoveTo(move, board))
+            {
+                yield return new NormalMove(from, move);
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return ForwardMove(from, board);
         }
     }
 }
