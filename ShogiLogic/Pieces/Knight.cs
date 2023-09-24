e45ff83b4d9d5e15004bc5d7c4b2fe61e4626145
Elijah Smith <elijah.d.s@outlook.com>
2023-09-24 08:28:46 -0400

    knight class updated

--- ShogiLogic/Pieces/Knight.cs
+++ ShogiLogic/Pieces/Knight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,41 @@ namespace ShogiLogic.Pieces
             Knight copy = new Knight(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> PotentialToPositions(Position from)
+        {
+            if (Color == Player.White)
+            {
+                foreach (Direction vDir in new Direction[] { Direction.North })
+                {
+                    foreach (Direction hDir in new Direction[] { Direction.East, Direction.West })
+                    {
+                        yield return from + 2 * vDir + hDir;
+                    }
+                }
+            }
+
+            else if (Color == Player.Black)
+            {
+                foreach (Direction vDir in new Direction[] { Direction.South })
+                {
+                    foreach (Direction hDir in new Direction[] { Direction.East, Direction.West })
+                    {
+                        yield return from + 2 * vDir + hDir;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from).Where(pos => Board.IsInside(pos) && (board.IsEmpty(pos) || board[pos].Color != Color));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositions(from, board).Select(to => new NormalMove(from, to));
         }
     }
 }
