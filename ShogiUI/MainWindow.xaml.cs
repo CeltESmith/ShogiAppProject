5d153d1b145c424ec437321054e841610f8a953e
Elijah Smith <elijah.d.s@outlook.com>
2023-09-24 08:26:31 -0400

    creating the and modifying the xaml.cs file

--- ShogiUI/MainWindow.xaml.cs
+++ ShogiUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ShogiLogic;
 
 namespace ShogiUI
 {
@@ -20,9 +21,42 @@ namespace ShogiUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[9,9];
+
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int r=0; r < 9; r++)
+            {
+                for(int c=0; c<9; c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int r=0;r < 9; r++)
+            {
+                for (int c=0; c<9; c++)
+                {
+                    Piece piece = board[r, c];
+                    pieceImages[r, c].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
