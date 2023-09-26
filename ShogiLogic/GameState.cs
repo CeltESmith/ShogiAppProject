5240dda1e7c968fbd42e2c2c501a4cb664422001
Elijah Smith <elijah.d.s@outlook.com>
2023-09-25 22:19:07 -0400

    Modifications to the GameState class

--- ShogiLogic/GameState.cs
+++ ShogiLogic/GameState.cs
@@ -11,10 +11,27 @@ namespace ShogiLogic
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
 
-        public GameState(Board board, Player player)
+        public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
             Board = board;
+        }
+
+        public IEnumerable<Move> LegalMovesForPiece(Position pos)
+        {
+            if (Board.IsEmpty(pos) || Board[pos].Color != CurrentPlayer)
+            {
+                return Enumerable.Empty<Move>();
+            }
+
+            Piece piece = Board[pos];
+            return piece.GetMoves(pos, Board);
+        }
+
+        public void MakeMove(Move move)
+        {
+            move.Execute(Board);
+            CurrentPlayer = CurrentPlayer.Opponent();
         }
     }
 }
