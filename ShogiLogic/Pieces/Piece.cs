4afdfcf9cc00c59d1d77f99445fda24a37f7bc2d
Elijah Smith <elijah.d.s@outlook.com>
2023-09-25 22:18:35 -0400

    Piece base class

--- ShogiLogic/Pieces/Piece.cs
+++ ShogiLogic/Pieces/Piece.cs
@@ -13,5 +13,58 @@ namespace ShogiLogic
         public bool HasMoved { get; set; } = false;
 
         public abstract Piece Copy();
+
+        public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+
+        protected IEnumerable<Position> MovePositionsInDir(Position from, Board board, Direction dir)
+        {
+            for (Position pos = from + dir; Board.IsInside(pos); pos += dir)
+            {
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+
+                Piece piece = board[pos];
+
+                if (piece.Color != Color)
+                {
+                    yield return pos;
+                }
+
+                yield break;
+            }
+        }
+
+        protected IEnumerable<Position> MovePositionsInDirs(Position from, Board board, Direction[] dirs)
+        {
+            return dirs.SelectMany(dir => MovePositionsInDir(from, board, dir));
+        }
+
+        protected IEnumerable<Position> MoveMulti(Position from, Board board, Direction[] dirs, Direction dir)
+        {
+
+            for (Position pos = from + dir; Board.IsInside(pos); pos += dir)
+            {
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+
+                Piece piece = board[pos];
+
+                if (piece.Color != Color)
+                {
+                    yield return pos;
+                }
+
+                yield break;
+            }
+
+            IEnumerable<Position> pos2 = dirs.SelectMany(dir => MovePositionsInDir(from, board, dir));
+            yield return (Position)pos2;
+        }
     }
 }
