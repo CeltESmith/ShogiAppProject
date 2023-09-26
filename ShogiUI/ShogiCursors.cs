ab8fcce617e79ad8da366d0597194a43a94d6361
Elijah Smith <elijah.d.s@outlook.com>
2023-09-25 22:22:53 -0400

    update to the Shogi cursor class

---
+++ ShogiUI/ShogiCursors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ShogiUI
+{
+    public static class ShogiCursors
+    {
+        public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");
+
+        private static Cursor LoadCursor(string filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
+            return new Cursor(stream, true);
+        }
+    }
+}
