18fc82f2c9449af96960c754686c5d1768ef4649
Elijah Smith <elijah.d.s@outlook.com>
2023-10-08 20:53:24 -0400

    Setting up a class to check and set result status.

---
+++ ShogiLogic/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLogic
+{
+	public class Result
+	{
+		public Player Winner { get; set; }
+		public EndReason Reason { get; }
+
+		public Result(Player winner, EndReason reason)
+		{
+			Winner = winner;
+			Reason = reason;
+		}
+
+		public static Result Win(Player winner)
+		{
+			return new Result(winner, EndReason.Checkmate);
+		}
+
+		public static Result Draw(EndReason reason)
+		{
+			return new Result(Player.None, reason);
+		}
+	}
+}
