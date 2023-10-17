c72398d86b5bb02bd41e4a7ee3b7741f816dd9af
Elijah Smith <elijah.d.s@outlook.com>
2023-10-17 09:21:01 -0400

    updates to GameOverMenu XAML CS file

--- ShogiUI/GameOverMenu.xaml.cs
+++ ShogiUI/GameOverMenu.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ShogiLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +22,58 @@ namespace ShogiUI
 	/// </summary>
 	public partial class GameOverMenu : UserControl
 	{
-		public GameOverMenu()
+		public event Action<Option> OptionSelected;
+
+		public GameOverMenu(GameState gameState)
 		{
 			InitializeComponent();
+
+			Result result = gameState.Result;
+			WinnerText.Text = GetWinnerText(result.Winner);
+			ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+		}
+
+		private static string GetWinnerText(Player winner)
+		{
+			return winner switch
+			{
+				Player.White => "WHITE WINS!",
+				Player.Black => "BLACK WINS!",
+				_ => "IT'S A DRAW!"
+			};
+		}
+
+		private static string PlayerString(Player player)
+		{
+			return player switch
+			{
+				Player.White => "White",
+				Player.Black => "Black",
+				_ => ""
+			};
+		}
+
+		private static string GetReasonText(EndReason reason, Player currentPlayer)
+		{
+			return reason switch
+			{
+				EndReason.Stalemate => $"STALEMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+				EndReason.Checkmate => $"CHECKMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+				//EndReason.FiftyMoveRule => "FIFTY_MOVE RULE",
+				//EndReason.InsufficientMaterial => "INSUFFICIENT MATERIAL",
+				//EndReason.ThreeFoldRepetition => "THREEFOLD REPETITION", 
+				_ => ""
+			};
+		}
+
+		private void Restart_Click(object sender, RoutedEventArgs e)
+		{
+			OptionSelected?.Invoke(Option.Restart);
 		}
 
 		private void Exit_Click(object sender, RoutedEventArgs e)
 		{
-
+			OptionSelected?.Invoke(Option.Exit);
         }
     }
 }
